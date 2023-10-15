bc99f59bf3b9d69e0f9763c5279b098c99a0aa3a
takumi <hello.camiha@gmail.com>
2023-10-15 17:15:17 +0900

    update styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import {  useState } from "react";
+import { useState } from "react";
 import { useFileDnD } from "./hooks/useFileDnD";
-import { emit } from '@tauri-apps/api/event'
+import { emit } from "@tauri-apps/api/event";
 
-import { css } from '../styled-system/css';
+import { css } from "../styled-system/css";
 
 function App() {
 	const [inputPaths, setInputPaths] = useState<Set<string>>(new Set());
@@ -15,18 +15,53 @@ function App() {
 				continue;
 			}
 			setInputPaths((prev) => new Set([...prev, arg]));
-			emit('front-to-back', arg);
+			emit("front-to-back", arg);
 		}
 	});
 
 	return (
-		<div className={css({})}>
-			<h1>drop here image!</h1>
-			<ul>
-				{Array.from(inputPaths).map((cur) => (
-					<li key={cur}>{cur}</li>
-				))}
-			</ul>
+		<div
+			className={css({
+				padding: 8,
+				display: "flex",
+				flexDirection: "column",
+				gap: 4,
+			})}
+		>
+			<div
+				className={css({
+					display: "flex",
+					flexDirection: "column",
+					gap: 2,
+				})}
+			>
+				<h1
+					className={css({
+						fontSize: "xx-large",
+						fontWeight: "bold",
+					})}
+				>
+					drop image here!
+				</h1>
+				<p className={css({})}>{"dnd png/jpeg => webp converter."}</p>
+			</div>
+
+			<div className={css({
+				width: "100%",
+				height: 0.5,
+				backgroundColor: "white"
+			})} />
+
+			<div>
+				<h2>files:</h2>
+				<ul>
+					{Array.from(inputPaths).map((cur) => (
+						<li key={cur}>
+							<div>{cur}</div>
+						</li>
+					))}
+				</ul>
+			</div>
 		</div>
 	);
 }
