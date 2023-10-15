8167eadd7ac69179cf87e6f3c26c28cd088a076e
takumi <hello.camiha@gmail.com>
2023-10-15 20:42:30 +0900

    migrate chakra

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,9 @@ export default defineConfig(async () => ({
 	server: {
 		port: 1420,
 		strictPort: true,
+		watch: {
+			usePolling: true,
+		},
 	},
 	// 3. to make use of `TAURI_DEBUG` and other env variables
 	// https://tauri.app/v1/api/config#buildconfig.beforedevcommand
