b1a81d2578142b3348b78e18fb4e83bbbcc9169a
takumi <hello.camiha@gmail.com>
2023-10-21 23:14:39 +0900

    refactor content template

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -6,9 +6,49 @@ import {
     Spinner,
 } from "@chakra-ui/react";
 import { CheckIcon, CloseIcon } from "@chakra-ui/icons";
-import type { Image } from "../hooks/use-image-file-drop";
+import type { Image, ImageItem } from "../hooks/use-image-file-drop";
 
-const ContentItem = ({ image }: { image: Image }) => {};
+const ContentItem = ({ image }: { image: ImageItem }) => {
+
+    return (
+        <ListItem
+            display={"flex"}
+            gap={2}
+            flexDirection={"column"}
+        >
+            <Flex gap={2} justifyContent={"space-between"}>
+                <Flex gap={2}>
+                    <>
+                        <Flex
+                            flexDirection={"column"}
+                            justifyContent={"center"}
+                            alignItems={"center"}
+                        >
+                            {image.isProgress ? (
+                                <Spinner size="xs" />
+                            ) : image.isFailed === true ?
+                                (
+                                    <CloseIcon fontSize={"xs"} color={"red.500"} />
+                                ) : (
+                                    <CheckIcon fontSize={"xs"} color={"green.500"} />
+                                )}
+                        </Flex>
+                        {image.isProgress ? (
+                            <Text fontSize={"xs"}>{image.fileName}</Text>
+                        ) : image.isFailed === true ? (
+                            <Text fontSize={"xs"}>{image.fileName} ({image.message})</Text>
+                        ) : (
+                            <Text fontSize={"xs"}>{image.fileName}</Text>
+                        )}
+                    </>
+                </Flex>
+                <Text fontSize={"xs"}>
+                    {image.reductionRate === 0 ? "-" : image.reductionRate}%
+                </Text>
+            </Flex>
+        </ListItem>
+    )
+};
 
 export const Content = ({ images }: { images: Image }) => {
     return (
@@ -19,43 +59,7 @@ export const Content = ({ images }: { images: Image }) => {
             </Flex>
             <List display="flex" flexDirection="column" gap={1}>
                 {Object.entries(images).map(([key, image]) => (
-                    <ListItem
-                        key={key}
-                        display={"flex"}
-                        gap={2}
-                        flexDirection={"column"}
-                    >
-                        <Flex gap={2} justifyContent={"space-between"}>
-                            <Flex gap={2}>
-                                <>
-                                    <Flex
-                                        flexDirection={"column"}
-                                        justifyContent={"center"}
-                                        alignItems={"center"}
-                                    >
-                                        {image.isProgress ? (
-                                            <Spinner size="xs" />
-                                        ) : image.isFailed === true ?
-                                            (
-                                                <CloseIcon fontSize={"xs"} color={"red.500"} />
-                                            ) : (
-                                                <CheckIcon fontSize={"xs"} color={"green.500"} />
-                                            )}
-                                    </Flex>
-                                    {image.isProgress ? (
-                                        <Text fontSize={"xs"}>{image.fileName}</Text>
-                                    ) : image.isFailed === true ? (
-                                        <Text fontSize={"xs"}>{image.fileName} ({image.message})</Text>
-                                    ) : (
-                                        <Text fontSize={"xs"}>{image.fileName}</Text>
-                                    )}
-                                </>
-                            </Flex>
-                            <Text fontSize={"xs"}>
-                                {image.reductionRate === 0 ? "-" : image.reductionRate}%
-                            </Text>
-                        </Flex>
-                    </ListItem>
+                    <ContentItem key={key} image={image} />
                 ))}
             </List>
         </Flex>
