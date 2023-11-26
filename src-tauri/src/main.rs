2879a94b9405e88415ea8d7d003fd6f1c6e33144
takumi <hello.camiha@gmail.com>
2023-11-26 16:38:10 +0900

    save config on rust

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,16 +4,18 @@
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 
 use image;
+use serde_json::json;
 use std::fs;
+use std::path::PathBuf;
 use webp;
 use webp::Encoder;
+
+use tauri_plugin_store::StoreBuilder;
 
 #[derive(Debug, serde::Deserialize)]
 struct ImageInputInfo {
     input_path: String,
     output_path: String,
-    lossless: bool,
-    quality: f32,
 }
 
 #[derive(Debug, serde::Serialize)]
@@ -23,19 +25,64 @@ struct ImageInfo {
     message: String,
 }
 
+#[derive(serde::Deserialize, serde::Serialize)]
+struct EncodeOption {
+    quality: f32,
+    lossless: bool,
+}
+
 #[tauri::command]
-async fn convert_webp(image_input_info: ImageInputInfo) -> ImageInfo {
+fn load_encode_option(app_handle: tauri::AppHandle) -> EncodeOption {
+    let store = StoreBuilder::new(app_handle.clone(), ".settings.json".into()).build();
+
+    let quality: f32 = store
+        .get("quality")
+        .map(|v| v.as_f64().unwrap_or(75.0) as f32)
+        .unwrap_or(75.0);
+    let lossless: bool = store
+        .get("lossless")
+        .map(|v| v.as_bool().unwrap_or(false))
+        .unwrap_or(false);
+
+    return EncodeOption { quality, lossless };
+}
+
+#[tauri::command]
+fn save_encode_option(encode_option: EncodeOption, app_handle: tauri::AppHandle) {
+    let mut store = StoreBuilder::new(app_handle.clone(), ".settings.json".into()).build();
+
+    if let Err(e) = store.insert("quality".to_string(), json!(encode_option.quality)) {
+        eprintln!("Failed to insert into store: {}", e);
+    }
+
+    if let Err(e) = store.insert("lossless".to_string(), json!(encode_option.lossless)) {
+        eprintln!("Failed to insert into store: {}", e);
+    }
+}
+
+#[tauri::command]
+async fn convert_webp(image_input_info: ImageInputInfo, app_handle: tauri::AppHandle) -> ImageInfo {
+    let store = StoreBuilder::new(app_handle.clone(), ".settings.json".into()).build();
+
+    let quality: f32 = store
+        .get("quality")
+        .map(|v| v.as_f64().unwrap_or(75.0) as f32)
+        .unwrap_or(75.0);
+    let lossless: bool = store
+        .get("lossless")
+        .map(|v| v.as_bool().unwrap_or(false))
+        .unwrap_or(false);
+
     let input_path = image_input_info.input_path;
     let output_path = image_input_info.output_path;
-    let lossless = image_input_info.lossless;
-    let quality = image_input_info.quality;
 
     if let Ok(img) = image::open(&input_path) {
         if let Ok(encoder) = Encoder::from_image(&img) {
             match encoder.encode_simple(lossless, quality) {
                 Ok(webp_output) => {
                     let output_path_slice: &[u8] = &webp_output;
-                    fs::write(&output_path, output_path_slice).expect("Failed to write WebP data to file");
+                    fs::write(&output_path, output_path_slice)
+                        .expect("Failed to write WebP data to file");
 
                     let input_size = fs::metadata(input_path)
                         .expect("Failed to get input file metadata")
@@ -50,7 +97,7 @@ async fn convert_webp(image_input_info: ImageInputInfo) -> ImageInfo {
                         output_size,
                         message: "Image successfully encoded and saved as WebP.".to_string(),
                     };
-                },
+                }
                 Err(_) => {
                     return ImageInfo {
                         input_size: 0,
@@ -77,8 +124,17 @@ async fn convert_webp(image_input_info: ImageInputInfo) -> ImageInfo {
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![convert_webp])
         .plugin(tauri_plugin_store::Builder::default().build())
+        .setup(|app| {
+            let path = PathBuf::from(".settings.json");
+            let mut _store = StoreBuilder::new(app.handle(), path).build();
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![
+            convert_webp,
+            load_encode_option,
+            save_encode_option
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,22 +1,10 @@
-import { Store } from "tauri-plugin-store-api";
 import { EncodeOption } from "../types";
+import { invoke } from "@tauri-apps/api/tauri";
 
 export const saveConfig = async (encodeOption: EncodeOption) => {
-	const encodeConfig = new Store(".settings.json");
-	const { lossless, quality } = encodeOption;
-
-	await encodeConfig.set("lossless", lossless);
-	await encodeConfig.set("quality", quality);
+	await invoke("save_encode_option", { encodeOption });
 };
 
 export const loadConfig = async (): Promise<EncodeOption> => {
-	const encodeConfig = new Store(".settings.json");
-	const lossless = await encodeConfig.get<boolean>("lossless");
-	const quality = await encodeConfig.get<number>("quality");
-
-	if (lossless == null || quality == null) {
-		return { lossless: false, quality: 75 };
-	}
-
-	return { lossless, quality };
+	return await invoke<EncodeOption>("load_encode_option", {});
 };
